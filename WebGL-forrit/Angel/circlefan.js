f43f1d284a0cd648e14e18bac6493ffd100ea1fd
katja8798 <katja8798@gmail.com>
2022-01-17 00:51:41 +0000

    circlefan breytt og klárað

--- WebGL-forrit/Angel/circlefan.js
+++ WebGL-forrit/Angel/circlefan.js
@@ -9,7 +9,7 @@ var gl;
 
 // numCirclePoints er fjöldi punkta á hringnum
 // Heildarfjöldi punkta er tveimur meiri (miðpunktur + fyrsti punktur kemur tvisvar)
-var numCirclePoints = 20;       
+var numCirclePoints = 50;
 
 var radius = 0.4;
 var center = vec2(0, 0);
@@ -49,14 +49,26 @@ window.onload = function init() {
 
 
 // Create the points of the circle
-function createCirclePoints( cent, rad, k )
-{
+//myndar runu: cent, p1,p2, cent, p2, p3, ect.... í points[]
+function createCirclePoints( cent, rad, k ) {
     var dAngle = 2*Math.PI/k;
+    var third = 1;//byrjum á cent ú rununi
+    var extraPoints = 0;//telur auka punktana sem baetast vid
     for( i=k; i>=0; i-- ) {
     	a = i*dAngle;
     	var p = vec2( rad*Math.sin(a) + cent[0], rad*Math.cos(a) + cent[1] );
     	points.push(p);
+        if (third >= 2) {//byrjum up á nýtt á runu
+            points.push(cent);//setjum miðju
+            points.push(p);//notum curr p svo ekkert gap myndast
+            third = 2;//byrjum tha strax í stodu nr 2
+            extraPoints += 2;
+        }
+        else {
+            third += 1;
+        }
     }
+    numCirclePoints += extraPoints;
 }
 
 function render() {
@@ -64,7 +76,7 @@ function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
     
     // Draw circle using Triangle Fan
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, numCirclePoints+2 );
+    gl.drawArrays( gl.TRIANGLES, 0, numCirclePoints+2 );
 
     window.requestAnimFrame(render);
 }
