afd575380762f232c11d43120ef56a6c59d2547b
katja8798 <katja8798@gmail.com>
2022-03-13 00:50:00 +0000

    Phong 100% lagað

--- WebGL-forrit/Angel/heimadaemi5/daemi5/PhongKula.js
+++ WebGL-forrit/Angel/heimadaemi5/daemi5/PhongKula.js
@@ -27,19 +27,19 @@ var fovy = 50.0;
 var near = 0.2;
 var far = 100.0;
 
-var va = vec4(0.0, 0.0, -1.0, 1);
+var va = vec4(0.0, 0.0, -1.0,1);
 var vb = vec4(0.0, 0.942809, 0.333333, 1);
 var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
-var vd = vec4(0.816497, -0.471405, 0.333333, 1);
+var vd = vec4(0.816497, -0.471405, 0.333333,1);
 
-var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
-var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
-var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
-var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
 
-var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
-var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
-var materialSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
 var materialShininess = 20.0;
 
 var ctm;
@@ -54,42 +54,43 @@ var eye;
 var at = vec3(0.0, 0.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
 
-let bhinn = false;
-    
-function triangle(a, b, c) {
-     
-     pointsArray.push(a);
-     pointsArray.push(b);      
-     pointsArray.push(c);
-    
-     // normals are vectors
-     
-     normalsArray.push(a[0],a[1], a[2], 0.0);
-     normalsArray.push(b[0],b[1], b[2], 0.0);
-     normalsArray.push(c[0],c[1], c[2], 0.0);
+// Upphafstilling, sýnum Phong fyrst
+let blong = false;
 
-     index += 3;
-     
+function triangle(a, b, c) {
+
+    pointsArray.push(a);
+    pointsArray.push(b);
+    pointsArray.push(c);
+
+    // normals are vectors
+
+    normalsArray.push(a[0],a[1], a[2], 0.0);
+    normalsArray.push(b[0],b[1], b[2], 0.0);
+    normalsArray.push(c[0],c[1], c[2], 0.0);
+
+    index += 3;
+
 }
 
 
 function divideTriangle(a, b, c, count) {
     if ( count > 0 ) {
-                
+
         var ab = mix( a, b, 0.5);
         var ac = mix( a, c, 0.5);
         var bc = mix( b, c, 0.5);
-                
+
         ab = normalize(ab, true);
         ac = normalize(ac, true);
         bc = normalize(bc, true);
-                                
+
         divideTriangle( a, ab, ac, count - 1 );
         divideTriangle( ab, b, bc, count - 1 );
         divideTriangle( bc, c, ac, count - 1 );
         divideTriangle( ab, bc, ac, count - 1 );
     }
-    else { 
+    else {
         triangle( a, b, c );
     }
 }
@@ -104,13 +105,13 @@ function tetrahedron(a, b, c, d, n) {
 
 window.onload = function init() {
 
-    canvas = document.getElementById("gl-canvas");
+    canvas = document.getElementById( "gl-canvas" );
 
-    gl = WebGLUtils.setupWebGL(canvas);
-    if (!gl) { alert("WebGL isn't available"); }
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0.9, 1.0, 1.0, 1.0);
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.9, 1.0, 1.0, 1.0 );
 
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
@@ -124,8 +125,8 @@ window.onload = function init() {
     //
     //  Load shaders and initialize attribute buffers
     //
-    var program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
 
 
     ambientProduct = mult(lightAmbient, materialAmbient);
@@ -139,101 +140,97 @@ window.onload = function init() {
     document.getElementById("NrVertices").innerHTML = index;
 
     var nBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
 
-    var vNormal = gl.getAttribLocation(program, "vNormal");
-    gl.vertexAttribPointer(vNormal, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vNormal);
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal);
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
 
-    var vPosition = gl.getAttribLocation(program, "vPosition");
+    var vPosition = gl.getAttribLocation( program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
-    projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
-    normalMatrixLoc = gl.getUniformLocation(program, "normalMatrix");
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+    normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
 
-    projectionMatrix = perspective(fovy, 1.0, near, far);
+    projectionMatrix = perspective( fovy, 1.0, near, far );
 
-    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition));
-    gl.uniform1f(gl.getUniformLocation(program, "shininess"), materialShininess);
+    gl.uniform4fv( gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition) );
+    gl.uniform1f( gl.getUniformLocation(program, "shininess"), materialShininess );
 
-    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct_bhinn"), flatten(ambientProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct_bhinn"), flatten(diffuseProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct_bhinn"), flatten(specularProduct));
-    gl.uniform1f(gl.getUniformLocation(program, "shininess_bhinn"), materialShininess);
-    gl.uniform1f(gl.getUniformLocation(program, "bhinn"), bhinn);
+    // Fyrir Blinn-Phong
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct_blinn"), flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct_blinn"), flatten(diffuseProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct_blinn"), flatten(specularProduct));
+    gl.uniform1f(gl.getUniformLocation(program, "shininess_blinn"), materialShininess);
+    gl.uniform1f(gl.getUniformLocation(program, "blong"), blong);
 
 
-    //event listeners for mouse
-    canvas.addEventListener("mousedown", function (e) {
+    document.getElementById("btnBlong").onclick = function () {
+        blong = !blong;
+        gl.uniform1f(gl.getUniformLocation(program, "blong"), blong);
+        if(!blong){
+            document.getElementById("btnBlong").innerText = "Blinn-Phong";
+        }else{
+            document.getElementById("btnBlong").innerText = "Phong";
+        }
+    };
+
+    document.getElementById("btnIncrease").onclick = function(){
+        if( numTimesToSubdivide < 7 ) numTimesToSubdivide++;
+        document.getElementById("Subdivisions").innerHTML = numTimesToSubdivide;
+        index = 0;
+        pointsArray = [];
+        normalsArray = [];
+        init();
+    };
+    document.getElementById("btnDecrease").onclick = function(){
+        if( numTimesToSubdivide > 0 ) numTimesToSubdivide--;
+        document.getElementById("Subdivisions").innerHTML = numTimesToSubdivide;
+        index = 0;
+        pointsArray = [];
+        normalsArray = [];
+        init();
+    };
+
+    // Event listeners for mouse
+    canvas.addEventListener("mousedown", function(e){
         movement = true;
         origX = e.clientX;
         origY = e.clientY;
         e.preventDefault();         // Disable drag and drop
-    });
+    } );
 
-    canvas.addEventListener("mouseup", function (e) {
+    canvas.addEventListener("mouseup", function(e){
         movement = false;
-    });
+    } );
 
-    canvas.addEventListener("mousemove", function (e) {
-        if (movement) {
-            spinY = (spinY + (e.clientX - origX)) % 360;
-            spinX = (spinX + (origY - e.clientY)) % 360;
+    canvas.addEventListener("mousemove", function(e){
+        if(movement) {
+            spinY = ( spinY + (e.clientX - origX) ) % 360;
+            spinX = ( spinX + (origY - e.clientY) ) % 360;
             origX = e.clientX;
             origY = e.clientY;
         }
-    });
-
-
-    document.getElementById("btnIncrease").onclick = function () {
-        if (numTimesToSubdivide < 7) numTimesToSubdivide++;
-        document.getElementById("Subdivisions").innerHTML = numTimesToSubdivide;
-        index = 0;
-        pointsArray = [];
-        normalsArray = [];
-        init();
-    };
-    document.getElementById("btnDecrease").onclick = function () {
-        if (numTimesToSubdivide > 0) numTimesToSubdivide--;
-        document.getElementById("Subdivisions").innerHTML = numTimesToSubdivide;
-        index = 0;
-        pointsArray = [];
-        normalsArray = [];
-        init();
-    };
-
-    document.getElementById("Bhinn-Phong").onclick = function () {
-        bhinn = true;
-        gl.uniform1f(gl.getUniformLocation(program, "bhinn"), bhinn);
-        document.getElementById("Phong").disabled = false;
-        document.getElementById("Bhinn-Phong").disabled = true;
-    };
-    document.getElementById("Phong").onclick = function () {
-        bhinn = false;
-        gl.uniform1f(gl.getUniformLocation(program, "bhinn"), bhinn);
-        document.getElementById("Bhinn-Phong").disabled = false;
-        document.getElementById("Phong").disabled = true;
-    };
-
+    } );
 
     // Event listener for mousewheel
-    window.addEventListener("wheel", function (e) {
-        if (e.deltaY > 0.0) {
+    window.addEventListener("wheel", function(e){
+        if( e.deltaY > 0.0 ) {
             zDist += 0.2;
         } else {
             zDist -= 0.2;
         }
-    });
+    }  );
 
     render();
 }
@@ -241,11 +238,11 @@ window.onload = function init() {
 
 function render() {
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    modelViewMatrix = lookAt(vec3(0.0, 0.0, zDist), at, up);
-    modelViewMatrix = mult(modelViewMatrix, rotate(spinY, [0, 1, 0]));
-    modelViewMatrix = mult(modelViewMatrix, rotate(spinX, [1, 0, 0]));
+    modelViewMatrix = lookAt( vec3(0.0, 0.0, zDist), at, up );
+    modelViewMatrix = mult( modelViewMatrix, rotate( spinY, [0, 1, 0] ) );
+    modelViewMatrix = mult( modelViewMatrix, rotate( spinX, [1, 0, 0] ) );
 
     // normal matrix only really need if there is nonuniform scaling
     // it's here for generality but since there is
@@ -256,11 +253,11 @@ function render() {
         vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
     ];
 
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
-    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
-    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
 
-    gl.drawArrays(gl.TRIANGLES, 0, index);
+    gl.drawArrays( gl.TRIANGLES, 0, index );
 
     window.requestAnimFrame(render);
 }
