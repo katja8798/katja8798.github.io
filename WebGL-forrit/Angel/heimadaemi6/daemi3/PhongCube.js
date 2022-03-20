16052446a1c48232f2e5fe952e4978d799198782
katja8798 <katja8798@gmail.com>
2022-03-20 01:14:51 +0000

    daemi 3 klarad

--- WebGL-forrit/Angel/heimadaemi6/daemi3/PhongCube.js
+++ WebGL-forrit/Angel/heimadaemi6/daemi3/PhongCube.js
@@ -25,7 +25,15 @@ var spinX = 0;
 var spinY = 0;
 var origX;
 var origY;
+var zDist = -5.0;
+var fovy = 60.0;
+var near = 0.2;
+var far = 100.0;
+var at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
 
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
 
 var matrixLoc;
 var normalMatrixLoc;
@@ -37,7 +45,7 @@ var diffuseColor = vec4(0.2, 0.0, 0.2, 1.0);
 var ambientColor = vec4(0.4, 0.1, 0.4, 1.0);
 
 var lightPosition = vec4(1.0, 1.0, 1.0, 1.0);
-var shininess = 50.0;
+var shininess = 150.0;
 
 window.onload = function init()
 {
@@ -75,8 +83,14 @@ window.onload = function init()
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
-    matrixLoc = gl.getUniformLocation( program, "rotation" );
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
     normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
+
+    //matrixLoc = gl.getUniformLocation( program, "rotation" );
+
+    projectionMatrix = perspective( fovy, 1.0, near, far );
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
 
     gl.uniform4fv(gl.getUniformLocation(program, "specularColor"), flatten(specularColor));
     gl.uniform4fv(gl.getUniformLocation(program, "ambientColor"), flatten(ambientColor));;
@@ -108,8 +122,7 @@ window.onload = function init()
     render();
 }
 
-function colorCube()
-{
+function colorCube() {
     quad( 1, 0, 3, 2 );
     quad( 2, 3, 7, 6 );
     quad( 3, 0, 4, 7 );
@@ -118,8 +131,7 @@ function colorCube()
     quad( 5, 4, 0, 1 );
 }
 
-function quad(a, b, c, d)
-{
+function quad(a, b, c, d) {
     const vertices = [
         vec3(-0.5, -0.5,  0.5),
         vec3(-0.5,  0.5,  0.5),
@@ -146,19 +158,19 @@ function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    let mv = mat4();
-    mv = mult( mv, rotateX(spinX) );
-    mv = mult( mv, rotateY(spinY) );
+    modelViewMatrix = lookAt( vec3(0.0, 0.0, zDist), at, up );
+    modelViewMatrix = mult( modelViewMatrix, rotateY( -spinY ) );
+    modelViewMatrix = mult( modelViewMatrix, rotateX( spinX ) );
 
     for (let i = 0; i < 3; i++) {
         let rows = [];
         for (let j = 0; j < 3; j++) {
-            rows.push(mv[i][j])
+            rows.push(modelViewMatrix[i][j])
         }
         normalMatrix.push(rows)
     }
 
-    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
 
     gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
